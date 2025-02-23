32eb4171c54b982cb4b4c020f9d727a651a00b8c
Hamza Saaf <hamzasaaf725@gmail.com>
2025-02-23 16:15:45 +0000

    Creation des toute megrations

--- project/database/migrations/2025_02_23_155429_create_clubs_table.php
+++ project/database/migrations/2025_02_23_155429_create_clubs_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateClubsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('clubs', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->enum('role', ['admin', 'étudiant'])->default('étudiant');
+            $table->text('description');
+            $table->string('category');
+            $table->string('logo')->nullable();
+            $table->softDeletes();
             $table->timestamps();
         });
     }
@@ -32,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('clubs');
     }
 }
